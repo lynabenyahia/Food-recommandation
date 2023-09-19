c19532ff4aef2a6a0151b45660efbab2e2465de1
HakimNessah <hakim.nessah@icloud.com>
2023-09-19 22:38:23 +0200

    Update newtop5.py

--- newtop5.py
+++ newtop5.py
@@ -23,6 +23,14 @@ api = API(
 
 categories = {
     "Produits laitiers, Desserts, Fromages": [
+        "Produits laitiers",
+        "Produits fermentés", 
+        "Produits laitiers fermentés", 
+        "Fromages", 
+        "Desserts lactés",
+        "Desserts lactés fermentés",
+        "Skyrs", 
+        "Fromages à la crème",
         "Dairies",
         "Desserts",
         "Cheeses",
@@ -74,27 +82,47 @@ def top_food():
     if user_category not in categories:
         messagebox.showerror("Erreur", "Catégorie non prise en charge.")
         return
-
+    
     cat_names = categories[user_category]
     products_list = []
+    if user_category == "Produits laitiers, Desserts, Fromages":
+        cat = "Produits laitiers, Desserts, Produits fermentés, Produits laitiers fermentés, Fromages, Desserts lactés, Desserts lactés fermentés, Skyrs, Fromages à la crème"
+    elif user_category == "Snacks sucrés, Confiseries":
+        cat = "Snacks, Snacks sucrés, Confiseries, Massepain, Pâtes d'amande"
+    elif user_category == "Viandes, Volailles":
+        cat = "Viandes, Volailles, Poulets, Aiguillettes de poulet"
+    elif user_category == "Produits de la mer, Poissons":
+        cat = "Produits de la mer, Poissons, Poissons gras, Saumons, Poissons fumés, Saumons fumés, Pavés de saumon, Pavés de saumon fumé"
+    elif user_category == "Petit-déjeuners":
+        cat = "Petit-déjeuners, Produits à tartiner, Produits à tartiner sucrés, Pâtes à tartiner, Pâtes à tartiner aux noisettes"
+    elif user_category == "Plats préparés":
+        cat = "Plats préparés, Plats préparés à réchauffer au micro-ondes"
+    elif user_category == "Boissons":
+        cat = "Boissons"
 
-    for cat_name in cat_names:
-        url = f"https://world.openfoodfacts.org/cgi/search.pl?search_terms={cat_name}&search_simple=1&action=process&json=1"
+    
+    
+    for cat in cat_names:
+        url = f"https://world.openfoodfacts.org/cgi/search.pl?search_terms={cat}&search_simple=1&action=process&json=1"
         response = requests.get(url)
         data = response.json()
 
-        if user_goal == "Moins de sucres":
-            data['products'].sort(key=lambda x: float(x.get('nutriments', {}).get('sugars_value', 0)))
-        elif user_goal == "Plus de protéines": 
-            data['products'].sort(key=lambda x: float(x.get('nutriments', {}).get('proteins_value', 0)), reverse=True)
-        elif user_goal == "Moins de calories":
-            data['products'].sort(key=lambda x: float(x.get('nutriments', {}).get('calories_value', 0)))
-        elif user_goal == "Moins de gras":
-            data['products'].sort(key=lambda x: float(x.get('nutriments', {}).get('fat_value', 0)))
-        elif user_goal == "Plus de fibres":
-            data['products'].sort(key=lambda x: float(x.get('nutriments', {}).get('fiber_value', 0)), reverse=True)
+        # Vérifiez que chaque produit appartient à la catégorie sélectionnée par l'utilisateur
+        for product in data['products']:
+            if cat in product.get('categories', []):
+                products_list.append(product)
 
-        products_list.extend(data['products'])
+    # Triez la liste de produits en fonction du contenu nutritionnel
+    if user_goal == "Moins de sucres":
+        products_list.sort(key=lambda x: float(x.get('nutriments', {}).get('sugars_100g', 0)))
+    elif user_goal == "Plus de protéines":
+        products_list.sort(key=lambda x: -float(x.get('nutriments', {}).get('proteins_100g', 0)))
+    elif user_goal == "Moins de calories":
+        products_list.sort(key=lambda x: float(x.get('nutriments', {}).get('energy-kcal_100g', 0)))
+    elif user_goal == "Moins de gras":
+        products_list.sort(key=lambda x: float(x.get('nutriments', {}).get('fat_100g', 0)))
+    elif user_goal == "Plus de fibres":
+        products_list.sort(key=lambda x: -float(x.get('nutriments', {}).get('fiber_100g', 0)))
 
     advice = f"Voici les cinq meilleurs produits qui correspondent à votre objectif :"
 
