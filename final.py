70018b23cef01cd62450feb389143321f518790f
lynabenyahia <122797987+lynabenyahia@users.noreply.github.com>
2023-09-21 10:56:45 +0200

    ajout du top 5

--- final.py
+++ final.py
@@ -17,6 +17,8 @@ from openfoodfacts import API, APIVersion, Country, Environment, Flavor
 from tkinter import PhotoImage
 import cv2
 from PIL import Image, ImageTk
+import requests
+from concurrent.futures import ThreadPoolExecutor
 
 # Fonction pour ouvrir la fenêtre de comparaison d'aliments
 def open_food_comparison_window():
@@ -153,6 +155,174 @@ def open_data_analysis_window():
     label2.image = image2  # Conserver une référence
     label2.pack()
 
+# Fonction pour ouvrir la fenêtre de top 5
+def open_top5_window():
+    # URL de l'API Open Food Facts pour la France
+    API_URL = "https://world.openfoodfacts.org/cgi/search.pl"
+
+    categories = {
+        "Produits laitiers, Desserts, Fromages": [
+            "Produits laitiers",
+            "Produits fermentés", 
+            "Produits laitiers fermentés", 
+            "Fromages", 
+            "Desserts lactés",
+            "Desserts lactés fermentés",
+            "Skyrs", 
+            "Fromages à la crème",
+            "Dairies",
+            "Desserts",
+            "Cheeses",
+        ],
+        "Snacks sucrés, Confiseries": [
+            "Snacks",
+            "Snacks sucrés",
+            "Confiseries",
+            "Massepain",
+            "Pâtes d'amande",
+        ],
+        "Viandes, Volailles": [
+            "Viandes",
+            "Volailles",
+            "Poulets",
+            "Aiguillettes de poulet",
+        ],
+        "Produits de la mer, Poissons": [
+            "Produits de la mer",
+            "Poissons",
+            "Poissons gras",
+            "Saumons",
+            "Poissons fumés",
+            "Saumons fumés",
+            "Pavés de saumon",
+            "Pavés de saumon fumé",
+        ],
+        "Petit-déjeuners": [
+            "Petit-déjeuners",
+            "Produits à tartiner",
+            "Produits à tartiner sucrés",
+            "Pâtes à tartiner",
+            "Pâtes à tartiner aux noisettes",
+        ],
+        "Plats préparés": ["Plats préparés", "Plats préparés à réchauffer au micro-ondes"],
+        "Boissons": ["Boissons"],
+    }
+
+    def produit_est_unique(product, products_list):
+        # Vérifie si le produit est déjà dans la liste des produits
+        for produit_existant in products_list:
+            if product['Produit'] == produit_existant['Produit']:
+                return False
+        return True
+
+    def recup_produit(cat):
+        params = {
+            "action": "process",
+            "tagtype_0": "categories",
+            "tag_contains_0": "contains",
+            "tag_0": cat,
+            "countries": "France",
+            "json": "1",
+            "page_size": 20,  # Vous pouvez augmenter ou diminuer selon vos besoins
+        }
+
+        response = requests.get(API_URL, params=params)
+
+        if response.status_code == 200:
+            data = response.json().get("products", [])
+            return data
+        return []
+
+    def top_food():
+        user_category = category_var.get()
+        user_goal = goal_var.get()
+
+        products_list = []
+
+        # Utiliser ThreadPoolExecutor pour paralléliser les requêtes API
+        with ThreadPoolExecutor(max_workers=5) as executor:
+            results = list(executor.map(recup_produit, categories[user_category]))
+
+        for data in results:
+            for produit in data:
+                product = {
+                    'Produit': produit.get('product_name'),
+                    'Marque': produit.get('brands',''),
+                    'Catégorie': produit.get('categories'),
+                    'Sucres': produit.get('nutriments', {}).get('sugars_100g', ''),
+                    'Graisses': produit.get('nutriments', {}).get('fat_100g', ''),
+                    'Calories': produit.get('nutriments', {}).get('energy-kcal_100g', ''),
+                    'Fibres': produit.get('nutriments', {}).get('fiber_100g', ''),
+                    'Protéines': produit.get('nutriments', {}).get('proteins_100g', ''),
+                }
+
+                # Vérifiez si le produit est unique, appartient à la catégorie 
+                if produit_est_unique(product, products_list) and product['Catégorie'] :
+                    products_list.append(product)
+
+        df = pd.DataFrame(products_list)
+        nutrient_column = ''  
+        if user_goal == "Moins de sucres":
+            nutrient_column = 'Sucres'
+        elif user_goal == "Plus de protéines":
+            nutrient_column = 'Protéines'
+        elif user_goal == "Moins de calories":
+            nutrient_column = 'Calories'
+        elif user_goal == "Moins de gras":
+            nutrient_column = 'Graisses'
+        elif user_goal == "Plus de fibres":
+            nutrient_column = 'Fibres'
+        
+        if nutrient_column:
+            df[nutrient_column] = pd.to_numeric(df[nutrient_column], errors='coerce')
+
+            df = df.dropna(subset=[nutrient_column])  
+            
+            
+            if user_goal == "Moins de sucres":
+                sorted_df = df.sort_values(by=nutrient_column, ascending=True)
+            elif user_goal == "Plus de protéines":
+                sorted_df = df.sort_values(by=nutrient_column, ascending=False)
+            elif user_goal == "Moins de calories":
+                sorted_df = df.sort_values(by=nutrient_column, ascending=True)
+            elif user_goal == "Moins de gras":
+                sorted_df = df.sort_values(by=nutrient_column, ascending=True)
+            elif user_goal == "Plus de fibres":
+                sorted_df = df.sort_values(by=nutrient_column, ascending=False)
+
+            top_5_products = sorted_df.head(5)
+            advice = f"Voici les cinq meilleurs produits qui correspondent à votre objectif :"
+            for i, products in enumerate(top_5_products.iterrows(), start=1):
+                advice += f"\n{i}. {products[1]['Produit']} - {products[1]['Marque']} - {products[1][nutrient_column]}g"
+
+            result_label.config(text=advice)
+        else:
+            result_label.config(text="Objectif invalide")
+
+    window = tk.Tk()
+    window.title("Recommandation d'aliments")
+
+    category_var = tk.StringVar()
+    category_var.set("Produits laitiers, Desserts, Fromages")  
+    category_label = tk.Label(window, text="Sélectionnez la catégorie d'aliments que vous recherchez :")
+    category_menu = tk.OptionMenu(window, category_var, "Produits laitiers, Desserts, Fromages", "Snacks sucrés, Confiseries", "Viandes, Volailles", "Produits de la mer, Poissons", "Petit-déjeuners", "Plats préparés", "Boissons")
+
+    goal_var = tk.StringVar()
+    goal_var.set("Moins de sucres") 
+    goal_label = tk.Label(window, text="Sélectionnez votre objectif:")
+    goal_menu = tk.OptionMenu(window, goal_var, "Moins de sucres", "Plus de protéines", "Moins de calories", "Moins de gras", "Plus de fibres")
+    compare_button = tk.Button(window, text="Chercher", command=top_food)
+    result_label = tk.Label(window, text="")
+
+    category_label.pack()
+    category_menu.pack()
+    goal_label.pack()
+    goal_menu.pack()
+    compare_button.pack()
+    result_label.pack()
+    window.mainloop()
+    
+    
 # Création de la fenêtre principale
 root = tk.Tk()
 root.title("Menu Principal")
@@ -160,9 +330,10 @@ root.title("Menu Principal")
 # Création du bouton pour ouvrir la fenêtre d'analyse de données
 button_data_analysis = tk.Button(root, text="Analyse de données", command=open_data_analysis_window)
 button_food_comparison = tk.Button(root, text="Comparaison d'aliments", command=open_food_comparison_window)
+button_top5 = tk.Button(root, text="Top 5 d'aliments", command=open_top5_window)
 button_data_analysis.pack()
 button_food_comparison.pack()
-button_data_analysis.pack()
+button_top5.pack()
 
 # Lancer la boucle
 root.mainloop()
